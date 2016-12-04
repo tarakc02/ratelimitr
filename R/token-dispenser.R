eb4da688eea9e16d5d786c80ab663403c710be3c
tarakc02 <tarakc02@github.com>
2016-12-04 14:14:00 -0800

    don't export token dispenser etc

--- R/token-dispenser.R
+++ R/token-dispenser.R
@@ -1,22 +1,3 @@
-#' Create a new token dispenser
-#'
-#' A token dispenser is an object with one method, \code{\link{request}}, which
-#' always returns TRUE, possibly after a delay. This function initializes a
-#' token dispenser with the given parameters. A token dispenser will never
-#' disburse more than \code{n} tokens in any window of length \code{period}.
-#'
-#' @param n Number of tokens disbursed in the given time period
-#' @param period Length of period in seconds
-#' @param precision Resolution with which to measure time, in fractions of a second
-#'
-#' @details
-#' In order to guarantee compliance with a rate limit, the token dispenser
-#' by default measures time intervals to the 1/60th of a second, always rounding
-#' down. Use the \code{precision} argument to specify what fraction of a second
-#' to use (60 means 1/60th, 100 means 1/100, etc).
-#'
-#' @import assertthat
-#' @export
 token_dispenser <- function(n, period, precision = 60) {
     # a constant to keep events from happening too close to the limit
     # (in 1 / precision seconds)
@@ -50,29 +31,8 @@ token_dispenser <- function(n, period, precision = 60) {
     structure(request, class = "token_dispenser")
 }
 
-#' Request a token from a token dispenser
-#'
-#' Once you've created a \code{\link{token_dispenser}}, use this function to
-#' request tokens. Tokens will be disbursed subject to the rate limit implied
-#' by the \code{token_dispenser}.
-#'
-#' @param x A \code{\link{token_dispenser}}
-#' @param policy A policy function (see details)
-#'
-#' @details
-#' A \code{policy} function specifies what to do if a function is called whose
-#' execution would violate the rate limit associated with the token dispenser.
-#' Policy functions take two arguments, the token dispenser and a
-#' \code{rate_limit_exception}, which is signalled by the token dispenser. The
-#' default policy, \code{wait}, sleeps for the necessary amount of time and then
-#' resubmits the request.
-#'
-#' @return TRUE (possibly after a delay)
-#' @export
 request <- function(x, policy = wait) UseMethod("request")
 
-#' @export
-#' @rdname request
 request.token_dispenser <- function(x, policy = wait) {
     tryCatch(
         x(),
