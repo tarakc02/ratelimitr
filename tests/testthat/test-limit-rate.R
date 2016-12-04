f0ae1dca7427d2fd45e0cdc2ae3eb146804387dc
tarakc02 <tarakc02@github.com>
2016-12-04 11:00:28 -0800

    use shorter limits on tests to speed up testing

--- tests/testthat/test-limit-rate.R
+++ tests/testthat/test-limit-rate.R
@@ -5,33 +5,33 @@ test_that("rate limited function does not exceed limits", {
 
     f_lim <- limit_rate(
         f,
-        rate(n = 10, period = .1),
-        rate(n = 50, period = 1),
+        rate(n = 10, period = .05),
+        rate(n = 40, period = .5),
         precision = 60
     )
 
     time11 <- system.time(replicate(11, f_lim()))[["elapsed"]]
-    expect_gt(time11, .1)
+    expect_gt(time11, .05)
 
     f_lim <- limit_rate(
         f,
-        rate(n = 10, period = .1),
-        rate(n = 50, period = 1),
+        rate(n = 10, period = .05),
+        rate(n = 40, period = .5),
         precision = 60
     )
 
-    time51 <- system.time(replicate(51, f_lim()))[["elapsed"]]
-    expect_gt(time51, 1)
+    time51 <- system.time(replicate(41, f_lim()))[["elapsed"]]
+    expect_gt(time51, .5)
 })
 
 test_that("rate-limited groups of functions obey rate limits", {
     f <- function() NULL
     g <- function() NULL
 
-    limited <- limit_rate(list(f = f, g = g), rate(n = 2, period = 1))
+    limited <- limit_rate(list(f = f, g = g), rate(n = 2, period = .1))
     evaltime <- system.time(
         {limited$f(); limited$g(); limited$f()}
     )[["elapsed"]]
 
-    expect_gt(evaltime, 1)
+    expect_gt(evaltime, .1)
 })
