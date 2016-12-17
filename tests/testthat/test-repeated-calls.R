1dab3849bed2ba4149106d2e375dd65873efac98
tarakc02 <tarakc02@users.noreply.github.com>
2016-12-16 17:14:59 -0800

    use reset in repeated tests (faster than sleeping)

--- tests/testthat/test-repeated-calls.R
+++ tests/testthat/test-repeated-calls.R
@@ -5,7 +5,7 @@ test_that("rates are consistently obeyed", {
     f_lim <- limit_rate(f, rate(n = 10, period = .03))
 
     logger <- function() {
-        Sys.sleep(.05)
+        f_lim <- reset(f_lim)
         system.time(replicate(11, f_lim()))[["elapsed"]]
     }
 
