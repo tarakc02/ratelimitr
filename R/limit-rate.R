14ff7c6f28b4195c4a428f42c233e289664780d0
tarakc02 <tarakc02@github.com>
2016-12-17 13:35:53 -0800

    update documentation

--- R/limit-rate.R
+++ R/limit-rate.R
@@ -14,13 +14,15 @@
 #' f <- limit_rate(Sys.time, rate(n = 5, period = .1))
 #' res <- replicate(10, f())
 #' ## show the elapsed time between each function call:
-#' res[-1] - head(res, -1)
+#' round(res[-1] - head(res, -1), 3)
 #'
 #' ## for multiple functions, make sure the list is named:
 #' f <- function() 1
 #' g <- function() 2
 #' limited <- limit_rate(list(f = f, g = g), rate(n = 1, period = .1))
 #' system.time({limited$f(); limited$g()})
+#'
+#' @seealso \code{\link{rate}}
 #'
 #' @name limit_rate
 #' @export
--- R/rate.R
+++ R/rate.R
@@ -10,6 +10,8 @@
 #' ## limit f to 10 calls per second
 #' limited_f <- limit_rate(f, rate(n = 10, period = 1))
 #'
+#' @seealso \code{\link{limit_rate}}
+#'
 #' @import assertthat
 #' @export
 rate <- function(n, period) {
