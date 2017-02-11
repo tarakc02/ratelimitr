6f7f6f6b21870f12e457ce3621743b60949dc602
tarakc02 <tarakc02@users.noreply.github.com>
2017-02-10 18:22:07 -0800

    update token dispenser to use proc.time instead of Sys.time fixes #11

--- R/token-dispenser.R
+++ R/token-dispenser.R
@@ -1,3 +1,7 @@
+time_now <- function()
+    #as.numeric(Sys.time())
+    proc.time()[["elapsed"]]
+
 token_dispenser <- function(n, period, precision = 60) {
     assert_that(is.count(n))
     assert_that(is.number(period))
@@ -6,17 +10,17 @@ token_dispenser <- function(n, period, precision = 60) {
     # So period (entered in seconds) is converted to period * precision
     period <- period * precision
 
-    init_time <- ceiling(as.numeric(Sys.time()) * precision)
+    init_time <- ceiling(time_now() * precision)
 
     tokens <- fixed_queue(n)
     replicate(n, push(tokens, init_time))
 
     request <- function() {
-        now <- floor(as.numeric(Sys.time()) * precision)
+        now <- floor(time_now() * precision)
         token <- front(tokens)
         if (now > token) {
             pop(tokens)
-            push(tokens, ceiling(as.numeric(Sys.time()) * precision) + period)
+            push(tokens, ceiling(time_now() * precision) + period)
             return(TRUE)
         }
 
