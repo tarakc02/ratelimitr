c06f4bc3096df348b5e57a119098114663ecc86c
tarakc02 <tarakc02@users.noreply.github.com>
2018-06-11 12:51:39 -0700

    implement UPDATE_RATE, fixes #15

---
+++ R/update-rate.R
@@ -0,0 +1,16 @@
+UPDATE_RATE <- function(lf, ..., precision = 60) {
+    gatekeeper_env <- parent.env(environment(lf))
+
+    rates <- list(...)
+    check_rates(rates)
+
+    gatekeepers <- lapply(rates, function(rate)
+        token_dispenser(
+            n = rate[["n"]],
+            period = rate[["period"]],
+            precision = precision)
+    )
+
+    assign("gatekeepers", gatekeepers, pos = gatekeeper_env)
+    invisible()
+}
