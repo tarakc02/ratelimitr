aba84ed4d6642ef14af6dc2585fa014f2d083f31
tarakc02 <tarakc02@github.com>
2018-06-11 09:19:41 -0700

    initial unit tests related to #15

---
+++ tests/testthat/test-update-rate.R
@@ -0,0 +1,22 @@
+context("updating rate limits")
+
+timer <- function(expr) {
+    round(system.time(expr)[["elapsed"]], 3)
+}
+
+test_that("can update rate of existing function, and it obeys the new rate", {
+    f <- function() NULL
+
+    f_lim <- limit_rate(
+        f,
+        rate(n = 5, period = .1),
+        precision = 60
+    )
+
+    tm <- timer(replicate(6, f_lim()))
+    expect_gt(tm, .1)
+
+    update_rate(f_lim, rate(n = 3, period = .1))
+    tm2 <- timer(replciate(4, f_lim()))
+    expect_gt(tm2, .1)
+})
